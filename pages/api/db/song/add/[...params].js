2c515bd97d54ab9af1dbc240f78518a730799fbf
Alex Lewin <alexlwn123@gmail.com>
2022-03-12 21:25:03 -0600

    adding/removing songs works

---
+++ pages/api/db/song/add/[...params].js
@@ -0,0 +1,24 @@
+import db from '../../../../../firebase/db'
+
+export default async function handler(req, res) {
+  const [songName, bid, spotifyUri] = req.query.params;
+  const docRef = db.collection('song-queue').doc(songName);
+  if (!(await docRef.get()).exists) { 
+    docRef.set({
+      bid: parseInt(bid),
+      songName: songName,
+      spotifyUri: spotifyUri
+    })
+  } else {
+    const currDoc = await (await docRef.get()).data();
+    await docRef.update({
+      bid: parseInt(currDoc.bid) + parseInt(bid),
+      songName: songName,
+      spotifyUri: spotifyUri
+    })
+  }
+    const newRef = await (await docRef.get()).data();
+    console.log(newRef);
+    res.status(200).json({success: true, song: newRef });
+
+}
