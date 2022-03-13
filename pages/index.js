7d89a94140e7a6911c3b3b3b25bb071cb8a903d1
Stephen DeLorme <stephen@d.elor.me>
2022-03-13 09:55:00 -0400

    Home title change

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
   return (
     <div className="h-screen">
       <Head>
-        <title>Pleb Party</title>
+        <title>PlebFM</title>
         <meta name="description" content="placeholder screen" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
