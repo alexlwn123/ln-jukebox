1e2fe0198c2d87282657e6205698298f415f5933
seent-asher <asher@seent.com>
2022-03-12 19:09:29 -0800

    Updated voltage.js w/ more LND api calls

--- pages/api/Voltage.js
+++ pages/api/Voltage.js
@@ -13,11 +13,31 @@ export default async function handler(req, res) {
 }
 
 // Create an invoice for a set amount of sats
-// @param lnd: Authenticated LND GRPC
-// @param amount: amount of the invoice in Satoshis
+// @param obj lnd: Authenticated LND GRPC
+// @param int amount: amount of the invoice in Satoshis
 // @return: string for LND invoice
 export async function makeInvoice(lnd, amount) {
   const {createInvoice} = require('ln-service');
   const invoice = await createInvoice({lnd, "amount":amount});
   return invoice.request;
+}
+
+// Fetch one invoice
+// @param lnd: Authenticated LND GRPC
+// @param id: amount of the invoice in Satoshis
+// @return bool status: status if invoice has been paid string for LND invoice
+export async function fetchInvoice(lnd, id) {
+  const {getInvoice} = require('ln-service');
+  const invoiceDetails = await getInvoice({id, lnd}); 
+  return invoiceDetails.is_confirmed;
+}
+
+// Fetches an array of all invoices
+// @param lnd: Authenticated LND GRPC
+// @param id: amount of the invoice in Satoshis
+// @return bool status: status if invoice has been paid string for LND invoice
+export async function fetchInvoices(lnd, id) {
+  const {getInvoices} = require('ln-service');
+  const {invoices} = await getInvoices({lnd});
+  return invoices;
 }
