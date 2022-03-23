f687940cfd3d696af6f793ea39f82236764e7731
Stephen DeLorme <stephen@d.elor.me>
2022-03-22 21:49:41 -0400

    Added streaming to leaderboard

--- components/Button.js
+++ components/Button.js
@@ -10,20 +10,26 @@ export default function Button(props){
   
   const ComponentName = components[ props.icon ];
   
+  const buildClasses = (size) => {
+    let base = "text-white border-solid border-2 border-white  w-full flex flex-row justify-center items-center uppercase tracking-[0.5em]"
+    let sizing = size === 'small' ?  "p-3 space-x-2 text-sm" : "p-6 space-x-4"
+    return base + " " + sizing
+  }
+  
   return(
     <div>
       { props.button ?
       <button
         type="button"
         onClick={props.onClick}
-        className="text-white border-solid border-2 border-white p-6 w-full flex flex-row justify-center items-center space-x-4 uppercase tracking-[0.5em]"
+        className={buildClasses(props.size)}
       >
         <span>{props.text}</span>
         <ComponentName icon={props.icon} className="w-8 h-8 text-white" />
       </button>
       :
       <a
-        className="text-white border-solid border-2 border-white p-6 w-full flex flex-row justify-center items-center space-x-4 uppercase tracking-[0.5em]"
+        className={buildClasses(props.size)}
         href={props.href}
       >
         <span>{props.text}</span>
